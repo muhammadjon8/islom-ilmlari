2c24e8155a2fc19218390c52f805345da5d94cca
muhammadjon8 <umuhammadjon22@gmail.com>
2025-10-12 23:53:52 +0500

    feat: added toaster

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import routes from "./routes";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={routes} />
+      <Toaster richColors position="top-right" />
     </Provider>
   );
 }
